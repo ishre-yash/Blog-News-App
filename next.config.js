cf2c2660eae0a2d9a20bbb70e7ebdfaa16e9e89c
shreyash <iamshreyash.in@gmail.com>
2022-03-20 17:18:33 +0530

    Day 04

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true
-}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  trailingSlash: true
+});
+
